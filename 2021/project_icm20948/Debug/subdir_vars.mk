0815e45fcc2b9f6d7f93fc2766bcc4e35a89f107
venki666 <vm@unlv.nevada.edu>
2021-10-14 10:10:51 -0700

    Add files via upload

---
+++ 2021/project_icm20948/Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../project0_ccs.cmd 
+
+C_SRCS += \
+../project0.c \
+../startup_ccs.c \
+/Users/venkatesanmuthukumar/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c 
+
+C_DEPS += \
+./project0.d \
+./startup_ccs.d \
+./uartstdio.d 
+
+OBJS += \
+./project0.obj \
+./startup_ccs.obj \
+./uartstdio.obj 
+
+OBJS__QUOTED += \
+"project0.obj" \
+"startup_ccs.obj" \
+"uartstdio.obj" 
+
+C_DEPS__QUOTED += \
+"project0.d" \
+"startup_ccs.d" \
+"uartstdio.d" 
+
+C_SRCS__QUOTED += \
+"../project0.c" \
+"../startup_ccs.c" \
+"/Users/venkatesanmuthukumar/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c" 
+
+
