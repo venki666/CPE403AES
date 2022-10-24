3d53f931546c3e4bb3c7600cee5e3fa075bb4832
venki666 <vm@unlv.nevada.edu>
2022-10-24 13:18:10 -0700

    Add files via upload

---
+++ 2022/mpu6050_Senslib/Debug/subdir_vars.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../blinky_ccs.cmd 
+
+C_SRCS += \
+../i2cm_drv.c \
+../main.c \
+../mpu6050.c \
+../tm4c123gh6pm_startup_ccs.c \
+/home/venki/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c 
+
+C_DEPS += \
+./i2cm_drv.d \
+./main.d \
+./mpu6050.d \
+./tm4c123gh6pm_startup_ccs.d \
+./uartstdio.d 
+
+OBJS += \
+./i2cm_drv.obj \
+./main.obj \
+./mpu6050.obj \
+./tm4c123gh6pm_startup_ccs.obj \
+./uartstdio.obj 
+
+OBJS__QUOTED += \
+"i2cm_drv.obj" \
+"main.obj" \
+"mpu6050.obj" \
+"tm4c123gh6pm_startup_ccs.obj" \
+"uartstdio.obj" 
+
+C_DEPS__QUOTED += \
+"i2cm_drv.d" \
+"main.d" \
+"mpu6050.d" \
+"tm4c123gh6pm_startup_ccs.d" \
+"uartstdio.d" 
+
+C_SRCS__QUOTED += \
+"../i2cm_drv.c" \
+"../main.c" \
+"../mpu6050.c" \
+"../tm4c123gh6pm_startup_ccs.c" \
+"/home/venki/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c" 
+
+
