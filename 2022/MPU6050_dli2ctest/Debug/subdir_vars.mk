84d7a6246769dada51dc4aaaafc2fc0374aba852
venki666 <vm@unlv.nevada.edu>
2022-10-26 13:21:19 -0700

    Add files via upload

---
+++ 2022/MPU6050_dli2ctest/Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../mpu6050_tst.cmd 
+
+C_SRCS += \
+../mpu6050_tst.c \
+../startup_ccs.c \
+/home/venki/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c 
+
+C_DEPS += \
+./mpu6050_tst.d \
+./startup_ccs.d \
+./uartstdio.d 
+
+OBJS += \
+./mpu6050_tst.obj \
+./startup_ccs.obj \
+./uartstdio.obj 
+
+OBJS__QUOTED += \
+"mpu6050_tst.obj" \
+"startup_ccs.obj" \
+"uartstdio.obj" 
+
+C_DEPS__QUOTED += \
+"mpu6050_tst.d" \
+"startup_ccs.d" \
+"uartstdio.d" 
+
+C_SRCS__QUOTED += \
+"../mpu6050_tst.c" \
+"../startup_ccs.c" \
+"/home/venki/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.c" 
+
+
---
+++ 2022/MPU6050_dli2ctest/mpu6050_tst.c
@@ -0,0 +1,144 @@
+#include <stdbool.h>
+#include "inc/tm4c123gh6pm.h"
+#include <stdint.h>
+#include "inc/hw_i2c.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/i2c.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "utils/uartstdio.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+#include <string.h>
+
+#include "mpu6050_def.h"
+
+#ifdef DEBUG
+void __error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+void initI2C0(void)
+{
+    // Turn on I2C0
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+    SysCtlDelay(3);
+    // Reset I2C0
+    SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
+    SysCtlDelay(3);
+    // Enable GPIOB
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+    SysCtlDelay(3);
+
+    // Configure GPIO SCL/SDA pins on PB2/PB3
+    GPIOPinConfigure(GPIO_PB2_I2C0SCL);
+    GPIOPinConfigure(GPIO_PB3_I2C0SDA);
+
+    // Set pins to I2C function
+    GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
+    GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
+
+    // Enable and master I2C
+    I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), false);
+
+    // Clear I2C FIFOs
+    HWREG(I2C0_BASE + I2C_O_FIFOCTL) = 80008000;
+}
+
+
+
+void I2C0Read(uint8_t slave_addr, uint8_t reg, uint8_t *data)
+{
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, false);
+    I2CMasterDataPut(I2C0_BASE, reg);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(I2CMasterBusy(I2C0_BASE));
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, true);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+    while(I2CMasterBusy(I2C0_BASE));
+    *data = I2CMasterDataGet(I2C0_BASE);
+}
+
+// This function has not been tested - for using 16bit read you can
+// also use the I2C0Read twice if this does not work
+void I2C0Read16(uint8_t slave_addr, uint8_t reg, uint16_t *data)
+{
+    uint8_t HByte , LByte=0;
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, false);
+    I2CMasterDataPut(I2C0_BASE, reg);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(I2CMasterBusy(I2C0_BASE));
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, true);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);
+    while(I2CMasterBusy(I2C0_BASE));
+    HByte = I2CMasterDataGet(I2C0_BASE);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT);
+    while(I2CMasterBusy(I2C0_BASE));
+    LByte = I2CMasterDataGet(I2C0_BASE);
+    *data = (LByte <<8 | HByte);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);
+    while(I2CMasterBusy(I2C0_BASE));
+}
+
+
+void I2C0Write(uint8_t slave_addr, uint8_t reg, uint8_t data)
+{
+    I2CMasterSlaveAddrSet(I2C0_BASE, slave_addr, false);
+    I2CMasterDataPut(I2C0_BASE, reg);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    while(I2CMasterBusy(I2C0_BASE));
+    I2CMasterDataPut(I2C0_BASE, data);
+    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);
+    while(I2CMasterBusy(I2C0_BASE));
+}
+
+/*reads the slave device*/
+void MPU_get_whom_am_I()
+{
+    uint8_t WAI=0;
+    I2C0Write(MPU6050_ADDRESS, MPU6050_REG_PWR_MGMT_1, 0x00);
+    SysCtlDelay(3);
+    I2C0Read(MPU6050_ADDRESS, MPU6050_REG_WHO_AM_I, &WAI);
+    if (WAI != 0x68)
+    UARTprintf("Device Not Found\n");
+    else
+    UARTprintf("Device Found\n");
+}
+
+/*Initializes the ICM20948 device*/
+void Init_MPU()
+{
+    UARTprintf("I2C Initialized\n");
+    SysCtlDelay(3);
+    MPU_get_whom_am_I();
+}
+
+
+int main(void)
+{
+    // Set clock to 40 MHz
+    SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
+
+    // Enable UART peripheral
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    // Configure UART GPIO
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+    UARTStdioConfig(0, 115200, 16000000);
+
+    //Init I2C
+    initI2C0();
+    Init_MPU();
+    while(1)
+    {}
+
+}
