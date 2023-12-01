841e3c0c0fd70758a33dff579e2c5e57b04d562c
venki666 <vm@unlv.nevada.edu>
2023-12-01 14:40:11 -0800

    Add files via upload

---
+++ 2023/simple_peripheral_CC1352R1_LAUNCHXL_tirtos7_ticlang/Release/Application/services/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Application/services/JoyStick.c 
+
+C_DEPS += \
+./Application/services/JoyStick.d 
+
+OBJS += \
+./Application/services/JoyStick.o 
+
+OBJS__QUOTED += \
+"Application\services\JoyStick.o" 
+
+C_DEPS__QUOTED += \
+"Application\services\JoyStick.d" 
+
+C_SRCS__QUOTED += \
+"../Application/services/JoyStick.c" 
+
+
---
+++ 2023/simple_peripheral_CC1352R1_LAUNCHXL_tirtos7_ticlang/Release/Application/subdir_vars.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/board_key.c \
+C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/rcosc/rcosc_calibration.c \
+../Application/simple_peripheral.c \
+../Application/simple_peripheral_menu.c \
+C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/menu/two_btn_menu.c \
+C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/util.c 
+
+C_DEPS += \
+./Application/board_key.d \
+./Application/rcosc_calibration.d \
+./Application/simple_peripheral.d \
+./Application/simple_peripheral_menu.d \
+./Application/two_btn_menu.d \
+./Application/util.d 
+
+OBJS += \
+./Application/board_key.o \
+./Application/rcosc_calibration.o \
+./Application/simple_peripheral.o \
+./Application/simple_peripheral_menu.o \
+./Application/two_btn_menu.o \
+./Application/util.o 
+
+OBJS__QUOTED += \
+"Application\board_key.o" \
+"Application\rcosc_calibration.o" \
+"Application\simple_peripheral.o" \
+"Application\simple_peripheral_menu.o" \
+"Application\two_btn_menu.o" \
+"Application\util.o" 
+
+C_DEPS__QUOTED += \
+"Application\board_key.d" \
+"Application\rcosc_calibration.d" \
+"Application\simple_peripheral.d" \
+"Application\simple_peripheral_menu.d" \
+"Application\two_btn_menu.d" \
+"Application\util.d" 
+
+C_SRCS__QUOTED += \
+"C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/board_key.c" \
+"C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/rcosc/rcosc_calibration.c" \
+"../Application/simple_peripheral.c" \
+"../Application/simple_peripheral_menu.c" \
+"C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/menu/two_btn_menu.c" \
+"C:/ti/simplelink_cc13xx_cc26xx_sdk_7_10_01_24/source/ti/ble5stack/common/cc26xx/util.c" 
+
+
